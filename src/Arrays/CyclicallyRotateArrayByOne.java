d91c7463f606bfb7c0ba9ccce06fb0a4583d3cf7
deepzsenu <deepzsenu@gmail.com>
2022-01-29 19:30:35 +0530

    Arrayss

---
+++ src/Arrays/CyclicallyRotateArrayByOne.java
@@ -0,0 +1,50 @@
+package Arrays;
+
+public class CyclicallyRotateArrayByOne {
+    public static void RotateByNByOneMethod(int[] arr, int d){
+        d = d%arr.length;
+        for(int i =0; i<d; i++){
+            RotateByOne(arr);
+        }
+
+    }
+    public static void RotateByNReverseMethod(int[] arr, int d){
+        int k = d%arr.length;
+        Reverse(arr, 0, arr.length-1);
+        Reverse(arr, 0, k-1);
+        Reverse(arr, k, arr.length-1);
+
+
+    }
+    public static void RotateByOne(int[] arr){
+        int n = arr.length;
+        int k = arr[n-1];
+        for (int i = n-2; i>=0; i--){
+            arr[i+1] = arr[i];
+        }
+        arr[0] = k;
+
+    }
+    public static void Reverse(int[] arr, int l, int h){
+        while(l <= h){
+            int temp = arr[l];
+            arr[l] = arr[h];
+            arr[h] = temp;
+            l++;
+            h--;
+        }
+    }
+    public static void PrintArray(int arr[]){
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2 ,3,4,5,6,7};
+//        RotateByOne(arr);
+        //RotateByNReverseMethod(arr, 2);
+        RotateByNByOneMethod(arr,2);
+        PrintArray(arr);
+    }
+}
--- src/Arrays/NegativeAtLeftSide.java
+++ src/Arrays/NegativeAtLeftSide.java
@@ -2,7 +2,25 @@ package Arrays;
 
 public class NegativeAtLeftSide {
     public static void siftNegativeToLeft(int[] arr, int n){
-
+        int  i =0;
+        int j = n-1;
+        while(i<=j){
+            if(arr[i]<0 && arr[j]<0){
+                i++;
+            }
+            else if(arr[i] >0 && arr[j]<0){
+                swap(arr, i,j);
+                i++;
+                j--;
+            }
+            else if(arr[i]>0 && arr[j]>0){
+                j--;
+            }
+            else{
+                j--;
+                i++;
+            }
+        }
 
     }
     public static void swap(int[] arr, int i, int j){
@@ -17,7 +35,7 @@ public class NegativeAtLeftSide {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,-3,1,2,3,4,-2,3};
+        int[] arr = {1,2,-3,-1,2,3,-4,-2,3};
         siftNegativeToLeft(arr,arr.length);
         PrintArray(arr);
 
